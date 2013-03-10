baa885ad6819505932bfd51ff358cde5d3d320f0
Joan Valduvieco <jvalduvieco@gmail.com>
2013-03-10 22:12:55 +0100

    Elimino código innecesario

--- code_examples/skvs/src/skvs_server_sup.erl
+++ code_examples/skvs/src/skvs_server_sup.erl
@@ -1,18 +1,13 @@
 -module(skvs_server_sup).
 -behaviour(supervisor).
 
--export([start_link/0, start_in_shell/0 ]).
+-export([start_link/0]).
 -export([init/1]).
 
 start_link() ->
 	% Initialize ETS table and link to supervsor process, making resistant to gen_server crashes
 	skvs_server:initialize(),
     supervisor:start_link(skvs_server_sup, []).
-
-start_in_shell() ->
-    {ok, Pid} = start_link(),
-    % Unlink the process from the shell
-    unlink(Pid).
 
 init(_Args) ->
     {ok, {{one_for_one, 1, 60},
